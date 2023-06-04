66897125edf1c9cee8e50bf7eac7c714a2b89dba
izupanda <izumi.handa@pandalab.jp>
2023-06-05 06:01:47 +0900

    add console.error

--- pages/api/generate-description.js
+++ pages/api/generate-description.js
@@ -3,25 +3,27 @@ import axios from 'axios';
 export default async function handler(req, res) {
   try {
     const { productName, marketplace, condition } = req.body;
-  
-    const chatGptResponse = await axios.post('https://api.openai.com/v1/chat/completions', {
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'user',
-          content: `あなたは私の代わりに${marketplace} にて${productName} を出品するための商品名と商品説明文を日本語で生成してください。${productName}の状態は${condition}です。商品名を出力する前は「商品名：」と出力してください。商品説明文を出力する前は「商品説明文：」と出力してください。なお、同時に複数のフリマサイトで出品しているため、購入前にコメントをもらうように注意文を記述してください。`,
-        },
-      ],
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-      }
-    });
-  } catch (err) {
-    console.error('Error in POST to OpenAI:', err);
-    // ...
-  }
-  
+    let chatGptResponse;
+
+    try {
+      chatGptResponse = await axios.post('https://api.openai.com/v1/chat/completions', {
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'user',
+            content: `あなたは私の代わりに${marketplace} にて${productName} を出品するための商品名と商品説明文を日本語で生成してください。${productName}の状態は${condition}です。商品名を出力する前は「商品名：」と出力してください。商品説明文を出力する前は「商品説明文：」と出力してください。なお、同時に複数のフリマサイトで出品しているため、購入前にコメントをもらうように注意文を記述してください。`,
+          },
+        ],
+      }, {
+        headers: {
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+        }
+      });
+    } catch (err) {
+      console.error('Error in POST to OpenAI:', err);
+      // Return from the function if there is an error.
+      return res.status(500).json({ error: err.message });
+    }
 
     const fullResponse = chatGptResponse.data.choices[0].message.content;
 
